ef24fe468a8a582f494ca537617b8b928858c1e2
Frank <12608674+FrankHitman@users.noreply.github.com>
2023-12-23 22:52:14 +0800

    add another fast solution for #380

--- algorithm/380-insert-delete-getrandom-o1.py
+++ algorithm/380-insert-delete-getrandom-o1.py
@@ -1,6 +1,6 @@
 import random
 
-
+# this solution consume more time than solution 2
 class RandomizedSet:
 
     def __init__(self):
@@ -20,8 +20,43 @@ class RandomizedSet:
         else:
             return False
 
+# according to https://stackoverflow.com/questions/40143157/big-o-complexity-of-random-choicelist-in-python3
+# the time complexity of random.choice is O(1) as well
     def getRandom(self) -> int:
         return random.choice(list(self.__array.keys()))
+
+# this solution is O(1), which is inspired by other people's submission
+class RandomizedSet2:
+
+    def __init__(self):
+        self.__hashmap = {}
+        self.__container=[]
+        self.__container_length = 0
+
+    def insert(self, val: int) -> bool:
+        if val in self.__hashmap:
+            return False
+        else:
+            self.__container.append(val)
+            self.__container_length += 1
+            self.__hashmap[val] = self.__container_length -1
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.__hashmap:
+            index = self.__hashmap[val]
+
+            self.__container[index] = self.__container[-1]
+            self.__hashmap[self.__container[-1]] = index
+            del self.__hashmap[val]
+            self.__container.pop()
+            self.__container_length -= 1
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        return self.__container[random.randrange(0, self.__container_length, 1)]
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
